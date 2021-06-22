276e279971eb4731274e9589f3a3ee6eeb36be2b
zzApotheosis <zzApotheosis@gmail.com>
2021-06-22 18:45:16 -0500

    Solve Day 00
    Signed-off-by: zzApotheosis <zzApotheosis@gmail.com>

--- 30 Days of Code - Rust/day00/src/main.rs
+++ 30 Days of Code - Rust/day00/src/main.rs
@@ -1,3 +1,14 @@
+/*
+ * Created by Steven Jennings on 22 June 2021.
+ */
+
+/* Imports */
+use std::io::{self, BufRead};
+
+/* Main */
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+    io::stdin().lock().read_line(&mut buffer).unwrap();
+    println!("Hello, World.");
+    println!("{}", buffer.trim());
 }
