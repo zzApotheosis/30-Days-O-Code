5785c0716b569a4b42501032edf5ee619bbb9fa4
zzApotheosis <zzApotheosis@gmail.com>
2021-06-22 20:20:07 -0500

    Solve Day 01
    Signed-off-by: zzApotheosis <zzApotheosis@gmail.com>

--- 30 Days of Code - Rust/day01/src/main.rs
+++ 30 Days of Code - Rust/day01/src/main.rs
@@ -1,3 +1,34 @@
+/*
+ * Created by Steven Jennings on 22 June 2021.
+ */
+
+/* Imports */
+use std::io::{self, BufRead};
+
+/* Main */
 fn main() {
-    println!("Hello, world!");
+    /* Declare variables */
+    let i: i32 = 4i32;
+    let d: f64 = 4.0f64;
+    let s: std::string::String = "HackerRank ".to_string();
+    let i_in: i32;
+    let d_in: f64;
+    let s_in: std::string::String;
+    let mut buffer: std::string::String = String::new();
+    
+    /* Fetch stdin */
+    buffer.clear();
+    io::stdin().lock().read_line(&mut buffer).unwrap();
+    i_in = buffer.trim().parse::<i32>().unwrap();
+    buffer.clear();
+    io::stdin().lock().read_line(&mut buffer).unwrap();
+    d_in = buffer.trim().parse::<f64>().unwrap();
+    buffer.clear();
+    io::stdin().lock().read_line(&mut buffer).unwrap();
+    s_in = buffer.trim().clone().to_string();
+    
+    /* Write stdout */
+    println!("{}", i + i_in);
+    println!("{:.1}", d + d_in);
+    print!("{}{}", s, s_in);
 }
